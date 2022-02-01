f13f70cf591057d97c594e520b0352f355656ab3
PatriziaCrimi <crimi.patrizia@gmail.com>
2022-02-01 14:29:25 +0100

    Project Initialization

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+  // Variables and constants initialization
+  $title = 'PHP Dice Game';
+  $subtitle = 'Roll of the dice - Simulator';
+?>
+<!-- ............................. HTML ............................. -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Dice Game</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="page-wrapper">
+      <div class="container">
+        <div class="row">
+          <h1>
+            <?php echo $title; ?>
+          </h1>
+        </div>
+        <div class="row">
+          <h2>
+            <?php echo $subtitle; ?>
+          </h2>
+        </div>
+        <div class="row">
+          <!-- Printing data -->
+          <div id="data-info">
+            <h3>Data Information</h3>
+            <p>
+              DATA HERE
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <!-- Results -->
+          <div id="results">
+            <h3>Results</h3>
+            <p class="message">
+              RESULTS HERE
+            </p>
+            <div class="results-box">
+              <p>
+                <span>
+                  RESULTS BOX HERE:
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>  <!-- End of container -->
+    </div>  <!-- End of page wrapper -->
+  </body>
+</html>
